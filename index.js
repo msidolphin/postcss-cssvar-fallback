f8440628c5f44a9815abc03a325a3977b3399ea1
msidolphin <msidolphin@outlook.com>
2023-07-11 20:20:46 +0800

    beta version

--- index.js
+++ index.js
@@ -1,31 +1,90 @@
+const fs = require('fs')
+const postcss = require('postcss')
+const valueParser = require('postcss-value-parser')
+
+const variables = new Map()
+
 /**
  * @type {import('postcss').PluginCreator}
  */
-module.exports = (opts = {}) => {
-  // Work with options here
-
-  return {
-    postcssPlugin: 'postcss-cssvar-fallback',
-    /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
-    */
-
-    /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
-
-    /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
+module.exports = postcss.plugin('postcss-cssvar-fallback', (options) => {
+  if (variables.size === 0) {
+    const { path } = options
+    const themes = fs.readFileSync(path, 'utf-8')
+    const root = postcss.parse(themes)
+    const nodes = root.nodes
+    // 收集cssvar
+    nodes.forEach(node => {
+      const Declarations = node.nodes
+      Declarations.forEach(Declaration => {
+        const prop = Declaration.prop
+        if (/^--/.test(prop)) {
+          variables.set(prop, Declaration.value)
+        }
+      })
+    })
+    variables.forEach((variable, key) => {
+      const ast = valueParser(variable)
+      if (ast && ast.nodes) {
+        ast.nodes.forEach(node => {
+          parseValue(node)
+        })
+        const parsedValue = ast.toString()
+        if (variable !== parsedValue) {
+          variables.set(key, parsedValue)
+        }
+      }
+    })
+  }
+  
+  function parseValue (node) {
+    if (node.type === 'word') {
+      const cssvar = node.value
+      if (/^--/.test(cssvar)) {
+        const parsedValue = variables.get(cssvar)
+        if (parsedValue) {
+          node.value = `${node.value}, ${parsedValue}`
+        }
+      }
+    } else {
+      if (node.nodes) {
+        if (node.nodes.length === 1) {
+          // 必然不包含默认值，要做处理
+          parseValue(node.nodes[0])
+        } else {
+          for (let i = 0; i < node.nodes.length; ++i) {
+            const curNode = node.nodes[i]
+            if (curNode.type === 'function') {
+              parseValue(curNode)
+            } else {
+              const defaultValue = node.nodes[i + 2]
+              if (!defaultValue) {
+                parseValue(curNode)
+              } else {
+                if (/^--/.test(defaultValue.value)) {
+                  parseValue(curNode)
+                }
+              }
+            }
+          }
+        }
       }
     }
-    */
   }
-}
+
+  return (root) => {
+    root.walkDecls((decl) => {
+      const value = decl.value
+      const ast = valueParser(value)
+      if (ast && ast.nodes) {
+        ast.nodes.forEach(node => {
+          parseValue(node)
+        })
+        decl.value = ast.toString()
+      }
+    })
+  }
+})
+
 
 module.exports.postcss = true
--- index.test.js
+++ index.test.js
@@ -1,17 +1,85 @@
 const postcss = require('postcss')
+const path = require('path')
 
 const plugin = require('./')
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+async function run (input, output) {
+  let result = await postcss([plugin({
+    path: path.join(__dirname, './test.theme.css')
+  })]).process(input, { from: undefined })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+it('test1', async () => {
+  await run(`
+    .test {
+      color: rgba(var(--kd-color-purple-10), 1);
+    }
+  `, `
+    .test {
+      color: rgba(var(--kd-color-purple-10, 102, 16, 59), 1);
+    }
+  `, { })
 })
 
-*/
+it(`test2`, async () => {
+  await run(`
+    .test {
+      color: var(--kd-color-public-normal);
+    }
+  `, `
+    .test {
+      color: var(--kd-color-public-normal, rgba(var(--kd-color-blue-6, 10, 108, 255), 1));
+    }
+  `)
+})
+
+it(`test3`, async () => {
+  await run(`
+    .test {
+      border: 1px sold var(--kd-color-public-normal);
+    }
+  `, `
+    .test {
+      border: 1px sold var(--kd-color-public-normal, rgba(var(--kd-color-blue-6, 10, 108, 255), 1));
+    }
+  `)
+})
+
+it(`test4`, async () => {
+  await run(`
+    .test {
+      border: var(--kd-font-size-small) sold var(--kd-color-public-normal);
+    }
+  `, `
+    .test {
+      border: var(--kd-font-size-small, 12px) sold var(--kd-color-public-normal, rgba(var(--kd-color-blue-6, 10, 108, 255), 1));
+    }
+  `)
+})
+
+it(`test5`, async () => {
+  await run(`
+    .test {
+      height: calc(var(--kd-font-size-small) + var(--kd-font-size-base));
+    }
+  `, `
+    .test {
+      height: calc(var(--kd-font-size-small, 12px) + var(--kd-font-size-base, 14px));
+    }
+  `)
+})
+
+it('ignore', async () => {
+  await run(`
+    .test {
+      color: var(--kd-color-line-brand, #000);
+    }
+  `, `
+    .test {
+      color: var(--kd-color-line-brand, #000);
+    }
+  `)
+})
